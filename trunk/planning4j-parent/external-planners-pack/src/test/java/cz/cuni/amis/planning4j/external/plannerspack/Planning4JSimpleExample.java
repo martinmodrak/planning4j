c0d8420b3fe7de20d33d31dc1ddf25bfbaf48059
cerny.m@gmail.com <cerny.m@gmail.com@1f571f35-c4f2-fe09-d748-d7bf82e52f33>
2012-03-29 15:42:08 +0000

    - Added example source - Modified linux version of FF and Metric-FF to report failure correctly

---
+++ trunk/planning4j-parent/external-planners-pack/src/test/java/cz/cuni/amis/planning4j/external/plannerspack/Planning4JSimpleExample.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (C) 2012 AMIS research group, Faculty of Mathematics and Physics, Charles University in Prague, Czech Republic
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package cz.cuni.amis.planning4j.external.plannerspack;
+
+import cz.cuni.amis.planning4j.ActionDescription;
+import cz.cuni.amis.planning4j.IDomainProvider;
+import cz.cuni.amis.planning4j.IPlanner;
+import cz.cuni.amis.planning4j.IPlanningResult;
+import cz.cuni.amis.planning4j.IProblemProvider;
+import cz.cuni.amis.planning4j.PlanningException;
+import cz.cuni.amis.planning4j.external.ExternalPlanner;
+import cz.cuni.amis.planning4j.external.impl.itsimple.ItSimplePlannerExecutor;
+import cz.cuni.amis.planning4j.external.impl.itsimple.ItSimplePlannerInformation;
+import cz.cuni.amis.planning4j.external.impl.itsimple.PlannerListManager;
+import cz.cuni.amis.planning4j.impl.PDDLDomainProvider;
+import cz.cuni.amis.planning4j.impl.PDDLProblemProvider;
+import cz.cuni.amis.planning4j.pddl.PDDLDomain;
+import cz.cuni.amis.planning4j.pddl.PDDLPredicate;
+import cz.cuni.amis.planning4j.pddl.PDDLProblem;
+import cz.cuni.amis.planning4j.pddl.PDDLRequirement;
+import cz.cuni.amis.planning4j.pddl.PDDLSimpleAction;
+import java.io.File;
+import java.util.EnumSet;
+import java.util.List;
+
+/**
+ *
+ * @author Martin Cerny
+ */
+public class Planning4JSimpleExample {
+  public static void main(String args[]) {
+   
+
+        /**
+         * Create a simple domain - two locations and an action to move between them
+         */
+        PDDLDomain domain = new PDDLDomain("test", EnumSet.of(PDDLRequirement.STRIPS));
+        domain.addPredicate(new PDDLPredicate("at_loc1"));
+        domain.addPredicate(new PDDLPredicate("at_loc2"));
+
+        PDDLSimpleAction moveAction = new PDDLSimpleAction("move");
+        moveAction.setPreconditionList("at_loc1");
+        moveAction.setPositiveEffects("at_loc2");
+        moveAction.setNegativeEffects("at_loc1");
+        domain.addAction(moveAction);
+
+        PDDLProblem problem = new PDDLProblem("problem_1", "test");
+        problem.setInitialLiterals("at_loc1");
+        problem.setGoalCondition("at_loc2");
+
+        IDomainProvider domainProvider = new PDDLDomainProvider(domain);
+        IProblemProvider problemProvider = new PDDLProblemProvider(problem);
+
+        /**
+         * Get a planner:
+         */
+        PlannerListManager plannerManager = PlannersPackUtils.getPlannerListManager();
+
+        //Let the engine suggest as a planner that supports strips and runs on current platform
+        List<ItSimplePlannerInformation> suggestedPlanners = plannerManager.suggestPlanners(PDDLRequirement.STRIPS);
+
+        if (suggestedPlanners.isEmpty()) {
+            System.out.println("No planner found for current platform.");
+            return;
+        }
+
+        //let's use the first suggested planner
+        ItSimplePlannerInformation plannerInfo = suggestedPlanners.get(0);
+
+        //To use a planner by name uncomment the following line
+        //ItSimplePlannerInformation plannerInfo = plannerManager.getPlannerByName("Metric-FF");
+
+       //This is the place to extract the planner
+        File plannersDirectory = new File("target");
+        //The planner is extracted (only if it does not exist yet) and exec permissions are set under Linux
+        plannerManager.extractAndPreparePlanner(plannersDirectory, plannerInfo);
+        
+        try {
+            IPlanner planner = new ExternalPlanner(new ItSimplePlannerExecutor(plannerInfo,plannersDirectory));            IPlanningResult result = planner.plan(domainProvider, problemProvider);
+            if (!result.isSuccess()) {
+                System.out.println("No solution found.");
+                return;
+            } else {
+                System.out.println("Found solution. The plan is:");
+                for (ActionDescription action : result.getPlan()) {
+                    System.out.println(action.getName());
+                }
+            }
+        } catch (PlanningException ex) {
+            System.out.println("Exception during planning.");
+            ex.printStackTrace();
+        }
+
+
+
+    }    
+}
