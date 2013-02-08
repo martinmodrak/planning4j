f57ac9571e16ceb49a666c9530afb014831c471e
cerny.m@gmail.com <cerny.m@gmail.com@1f571f35-c4f2-fe09-d748-d7bf82e52f33>
2013-02-08 21:03:51 +0000

    - Added substitution support to PDDLPredicate

--- planning4j-parent/planning4j-base/src/main/java/cz/cuni/amis/planning4j/pddl/PDDLPredicate.java
+++ planning4j-parent/planning4j-base/src/main/java/cz/cuni/amis/planning4j/pddl/PDDLPredicate.java
@@ -57,5 +57,21 @@ public class PDDLPredicate {
         return name + "/" + parameters.size();
     }
     
-    
+    public String stringAfterSubstitution(String ... substituents){
+        if(substituents.length != getParameters().size()){
+            throw new IllegalArgumentException("Predicate " + getName() + " requires " + getParameters().size() + " parameters, got " + substituents.length);
+        }
+        StringBuilder sb = new StringBuilder(getName());
+        if(getParameters().size() > 0){
+            sb.append("(");
+            for(int i = 0; i < substituents.length; i++){
+                if(i > 0){
+                    sb.append(",");
+                }
+                sb.append(substituents[i]);
+            }
+            sb.append(")");
+        }
+        return sb.toString();
+    }
 }
