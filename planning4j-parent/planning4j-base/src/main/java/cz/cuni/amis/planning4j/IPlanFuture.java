3b3f7cae728dc7760ee73eab39e43076433dd64b
cerny.m@gmail.com <cerny.m@gmail.com@1f571f35-c4f2-fe09-d748-d7bf82e52f33>
2013-02-22 11:16:50 +0000

    - Make use of IFutureWithListeners interface from amis-utils

--- planning4j-parent/planning4j-base/src/main/java/cz/cuni/amis/planning4j/IPlanFuture.java
+++ planning4j-parent/planning4j-base/src/main/java/cz/cuni/amis/planning4j/IPlanFuture.java
@@ -6,18 +6,15 @@ package cz.cuni.amis.planning4j;
 
 import cz.cuni.amis.utils.future.FutureStatus;
 import cz.cuni.amis.utils.future.IFutureListener;
+import cz.cuni.amis.utils.future.IFutureWithListeners;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 /**
  *
  * @author Martin Cerny
  */
-public interface IPlanFuture<RESULT extends IPlanningResult> extends Future<RESULT> {
-	/**
-	 * Current status of the plan computation.
-	 * @return
-	 */
-	public FutureStatus getStatus();
+public interface IPlanFuture<RESULT extends IPlanningResult> extends IFutureWithListeners<RESULT> {
 	
         /**
          * Overriden here to provide correct generic parameter binding
@@ -26,30 +23,33 @@ public interface IPlanFuture<RESULT extends IPlanningResult> extends Future<RESU
         @Override
         public RESULT get();
         
-	/**
-	 * Adds a listener on a future status (using strong reference). Listeners are automatically
-	 * removed whenever the future gets its result (or is cancelled or an exception happens).
-	 * @param listener
-	 */
-	public void addFutureListener(IFutureListener<RESULT> listener);
-	
-	/**
-	 * Removes a listener from the future.
-	 * @param listener
-	 */
-	public void removeFutureListener(IFutureListener<RESULT> listener);
-	
-	/**
-	 * Whether some listener is listening on the future.
-	 * @param listener
-	 * @return
-	 */
-	public boolean isListening(IFutureListener<RESULT> listener);
-        
         /**
-         * Gets excetption (if any) that terminated the computation of this future 
+         * Overriden here to provide correct generic parameter binding
          * @return 
          */
-        public Exception getException();
-    
+        @Override
+        RESULT get(long timeout, TimeUnit unit);        
+        
+        /**
+         * Overriden here to provide correct generic parameter binding
+         * @return 
+         */
+        @Override
+	public void addFutureListener(IFutureListener<RESULT> listener);
+	
+        /**
+         * Overriden here to provide correct generic parameter binding
+         * @return 
+         */
+        @Override
+	public void removeFutureListener(IFutureListener<RESULT> listener);
+	
+        /**
+         * Overriden here to provide correct generic parameter binding
+         * @return 
+         */
+        @Override
+	public boolean isListening(IFutureListener<RESULT> listener);
+        
+        
 }
